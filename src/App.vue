cf559a800af2f54870a2021394957f283eb4e84b
Victor Cordeiro <victor.cordeiru@proton.me>
2023-09-20 14:31:42 -0300

    add: components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+import XButton from './components/XButton.vue';
+import XTextField from './components/XTextField.vue';
+</script>
 
 <template>
   <main
@@ -11,6 +14,24 @@
         use seus Ethereums para enviar uma mensagem. Cada post enviado você terá
         chance de ganhar um valor de Ethereum de volta.
       </p>
+      <XButton text="Conectar carteira"/>
+      <XTextField label="Post" name="post" class="mb-2" type="text" id="post" aria-placeholder="Name..." required/>
+      <XButton text="Enviar post"/>
+      <div class="flex items-center">
+        <span class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900 h-fit">
+            Conectado!
+        </span>
+        <span class="truncate">
+            ....
+        </span>
+      </div>
+    </div>
+    <div
+      class="mt-8 rounded-md border border-gray-700 text-white bg-gray-800 p-6 mx-auto w-full max-w-[600px]"
+    >
+      <h1 class="text-white text-lg mb-4">Todos os posts</h1>
+      <div class="text-center mb-4">Carregando...</div>
+      <XPost address="123" timestamp="16:54" message="Descrição da mensagem" />
     </div>
   </main>
 </template>
---
+++ src/components/XPost.vue
@@ -0,0 +1,17 @@
+<script setup>
+//https://vuejs.org/guide/components/props.html
+defineProps({ //-> Props like React -> With TS??
+    address: String,
+    timestamp: String,
+    message: String,
+})
+</script>
+<template>
+    <div 
+    class="border border-gray-700 p-4 rounded mb-2">
+        <div class="truncate"> Carteira: {{ address }}</div>
+        <div> Data: {{  timestamp }}</div>
+        <div> Mensagem: {{ message }}</div>   
+    </div>
+
+</template>
